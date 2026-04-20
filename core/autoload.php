9c0e9a7de2bca8a7b8fbcefd24672be54edce31d
Quentin Pugeat <contact@quentinpugeat.fr>
2026-04-20 23:53:50 +0200

    Passage à la version 1.1

--- core/autoload.php
+++ core/autoload.php
@@ -6,4 +6,4 @@ require_once __DIR__ . "/ProjectElement.class.php";
 require_once __DIR__ . "/View/ViewHandler.php";
 
 define('REGIS_CORE', __DIR__ . '/');
-define('REGIS_VERSION', '1.0.0.0');
+define('REGIS_VERSION', '1.1.0.0');
